ea8e5e9747e2f4355cb3631f026a8ae41f14ea76
student <student@GBTC440J11LX.montgomerycollege.edu>
2018-01-25 09:10:05 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String  showIndex()
+    {
+        String userName="";
+        Scanner keyboard= new Scanner(System.in);
+        userName=keyboard.nextLine();System.out.println();
+        String greet= "Hello "+userName;
+        return greet;
+    }
+
+}
